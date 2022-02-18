13573d6ec9a63490a005a195b1360bc88e3e3866
darick_Mac <darickdavis@gmail.com>
2022-02-18 18:07:45 -0500

    Added 2 private lbs a public lb a public ip

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "azurerm_virtual_machine_scale_set" "V1VMSSReference" {
   }
 
 
-# Use Keyvault in this section
+
   os_profile {
     computer_name_prefix = var.VMComputername
     admin_username       = "azureuser"
@@ -88,7 +88,78 @@ resource "azurerm_virtual_machine_scale_set" "V1VMSSReference" {
 }
 
 
-#We want to make our Load Balancer Internal (Private) to go to our Busines Tier VMSS & SQL Server Always on
+#We want to make our Load Balancer Public to go to our Web Tier VMSS
+
+resource "azurerm_public_ip" "V1toWebPIP" {
+  name                = "PublicIPForLB1"
+  location            = "east US"
+  resource_group_name = azurerm_resource_group.RG.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "V1toWebLB" {
+  name                = "V1WLB"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress1"
+    public_ip_address_id = azurerm_public_ip.V1toWebPIP.id
+    
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "V1Pbpepool1" {
+  resource_group_name = azurerm_resource_group.RG.name
+  loadbalancer_id     = azurerm_lb.V1toWebLB.id
+  name                = "V1PBackEndAddressPool1"
+}
+
+resource "azurerm_lb_nat_pool" "V1Plbnatpool1" {
+  resource_group_name            = azurerm_resource_group.RG.name
+  name                           = "ssh"
+  loadbalancer_id                = azurerm_lb.V1toWebLB.id
+  protocol                       = "Tcp"
+  frontend_port_start            = 22
+  frontend_port_end              = 22
+  backend_port                   = 22
+  frontend_ip_configuration_name = "PublicIPAddress1"
+}
+
+resource "azurerm_lb_nat_pool" "V1Plbnatpool2" {
+  resource_group_name            = azurerm_resource_group.RG.name
+  name                           = "web"
+  loadbalancer_id                = azurerm_lb.V1toWebLB.id
+  protocol                       = "Tcp"
+  frontend_port_start            = 80
+  frontend_port_end              = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "PublicIPAddress1"
+}
+
+resource "azurerm_lb_nat_pool" "V1Plbnatpool3" {
+  resource_group_name            = azurerm_resource_group.RG.name
+  name                           = "SQL"
+  loadbalancer_id                = azurerm_lb.V1toWebLB.id
+  protocol                       = "Tcp"
+  frontend_port_start            = 1433
+  frontend_port_end              = 1433
+  backend_port                   = 1433
+  frontend_ip_configuration_name = "PublicIPAddress1"
+}
+
+resource "azurerm_lb_probe" "V1PHealthProbe1" {
+  resource_group_name = azurerm_resource_group.RG.name
+  loadbalancer_id     = azurerm_lb.V1toWebLB.id
+  name                = "http-probe"
+  protocol            = "Http"
+  request_path        = "/health"
+  port                = 80
+}
+
+
+
+#We want to make our Load Balancer Internal (Private) to go to our Busines Tier VMSS
 resource "azurerm_lb" "V1WebtoBusinessLB" {
   name                = "V1WbLB"
   location            = azurerm_resource_group.RG.location
@@ -103,26 +174,91 @@ resource "azurerm_lb" "V1WebtoBusinessLB" {
   }
 }
 
-resource "azurerm_lb_backend_address_pool" "bpepool" {
+resource "azurerm_lb_backend_address_pool" "V1bpepool1" {
   resource_group_name = azurerm_resource_group.RG.name
   loadbalancer_id     = azurerm_lb.V1WebtoBusinessLB.id
-  name                = "BackEndAddressPool"
+  name                = "V1BackEndAddressPool1"
 }
 
-resource "azurerm_lb_nat_pool" "lbnatpool" {
+resource "azurerm_lb_nat_pool" "V1lbnatpool1" {
   resource_group_name            = azurerm_resource_group.RG.name
   name                           = "ssh"
   loadbalancer_id                = azurerm_lb.V1WebtoBusinessLB.id
   protocol                       = "Tcp"
-  frontend_port_start            = 50000
-  frontend_port_end              = 50119
+  frontend_port_start            = 22
+  frontend_port_end              = 22
   backend_port                   = 22
   frontend_ip_configuration_name = "PrivateIPAddress1"
 }
 
-resource "azurerm_lb_probe" "HealthProbe" {
+resource "azurerm_lb_nat_pool" "V1lbnatpool2" {
+  resource_group_name            = azurerm_resource_group.RG.name
+  name                           = "web"
+  loadbalancer_id                = azurerm_lb.V1WebtoBusinessLB.id
+  protocol                       = "Tcp"
+  frontend_port_start            = 80
+  frontend_port_end              = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "PrivateIPAddress1"
+}
+
+resource "azurerm_lb_nat_pool" "V1lbnatpool3" {
+  resource_group_name            = azurerm_resource_group.RG.name
+  name                           = "SQL"
+  loadbalancer_id                = azurerm_lb.V1WebtoBusinessLB.id
+  protocol                       = "Tcp"
+  frontend_port_start            = 1433
+  frontend_port_end              = 1433
+  backend_port                   = 1433
+  frontend_ip_configuration_name = "PrivateIPAddress1"
+}
+
+resource "azurerm_lb_probe" "V1HealthProbe1" {
   resource_group_name = azurerm_resource_group.RG.name
   loadbalancer_id     = azurerm_lb.V1WebtoBusinessLB.id
+  name                = "http-probe"
+  protocol            = "Http"
+  request_path        = "/health"
+  port                = 80
+}
+
+
+#We want to make our Load Balancer Internal (Private) to go to our SQL database
+resource "azurerm_lb" "V1WebtoBusinessLB2" {
+  name                = "V1WbLB2"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+
+  frontend_ip_configuration {
+    name                 = "PrivateIPAddress2"
+    subnet_id = azurerm_subnet.VNet1Subnet.id
+    private_ip_address = "10.0.0.2"
+    private_ip_address_allocation = "static"
+    private_ip_address_version = "IPv4"
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "V1bpepool2" {
+  resource_group_name = azurerm_resource_group.RG.name
+  loadbalancer_id     = azurerm_lb.V1WebtoBusinessLB2.id
+  name                = "V1BackEndAddressPool2"
+}
+
+resource "azurerm_lb_nat_pool" "V1lbnatpool4" {
+  resource_group_name            = azurerm_resource_group.RG.name
+  name                           = "SQL"
+  loadbalancer_id                = azurerm_lb.V1WebtoBusinessLB2.id
+  protocol                       = "Tcp"
+  frontend_port_start            = 1433
+  frontend_port_end              = 1433
+  backend_port                   = 1433
+  frontend_ip_configuration_name = "PrivateIPAddress2"
+  floating_ip_enabled            = true
+}
+
+resource "azurerm_lb_probe" "V1HealthProbe1" {
+  resource_group_name = azurerm_resource_group.RG.name
+  loadbalancer_id     = azurerm_lb.V1WebtoBusinessLB2.id
   name                = "http-probe"
   protocol            = "Http"
   request_path        = "/health"
